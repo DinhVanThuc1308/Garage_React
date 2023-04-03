9638b6a6b3607ca9feb6a714cd8f9554862aebbe
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2023-04-03 10:55:47 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
 import { render } from '@testing-library/react';
 import Header from './components/Header/Header';
 import Slider_bar from './components/Slider_bar/Slider_bar';
-
+import ChangePassword from './components/ChangePassword';
+import './App.css';
 function App() {
   return (
-    <div className='container'>
-      <Header></Header>
-      <Slider_bar></Slider_bar>
+    <div className="container">
+      {/* <Header></Header>
+      <Slider_bar></Slider_bar> */}
+      {/* <ChangePassword ></ChangePassword> */}
     </div>
   );
 }

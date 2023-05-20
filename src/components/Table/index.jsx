93861341ffb3a6aae8755ef3b3163f7e16a1e569
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2023-05-21 03:06:11 +0700

    doneChangePW and doingGararaOnwer

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,14 +1,16 @@
 import { Space, Table } from 'antd';
-import eye from './assets/Icon/eye.png';
-import edit from './assets/Icon/Edit.png';
-import deleteIcon from './assets/Icon/Vector.png';
+import eye from '../Table/assets/Icon/eye.png';
+import edit from '../Table/assets/Icon/Edit.png';
+import deleteIcon from '../Table/assets/Icon/Vector.png';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axiosInstance from '../../shared/services/http-client.js';
 
 import { Button, Input, Select } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-// import { Link } from 'react-router-dom';
+import './style.css'
+
+
 
 const options = [
   {
@@ -28,42 +30,59 @@ const options2 = [
   {
     value: 'active',
     label: 'done',
-  },
+  }
 ];
 
 function App() {
   const [data, setData] = useState([]);
-  const [search, setSearch] = useState('');
-  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('')
+
+  const handleDelete = async (id) => {
+    // delete user
+    await axiosInstance.delete(`users/${id}`);
+    // call api
+    callApi();
+
+
+  }
 
   const callApi = async () => {
-    // const params = {
-    //     filters: {
-    //         $or: [
-    //             { name: { $contains: search } },
-    //             { email: { $contains: search } }
-    //         ],
-    //         owner: { id: { $eq: 1 } }
-    //     },
-    //     pagination: { page: page, pageSize: 10 },
-    //     populate: 'owner,services'
-    // };
 
-    const data = await axiosInstance.get('users');
+    const data = await axiosInstance.get('users',);
     console.log(data);
+
     const users = data.map(user => ({
       id: user.id,
       name: user.fullname,
       email: user.email,
       phoneNumber: user.phoneNumber,
       status: user.status === 'active' ? 'Active' : 'Inactive',
+      action: (
+        <Space key={user.id} size="middle">
+          <Link to="/management_details">
+            <img src={eye} style={{ width: '14.05px', height: '16.03px' }} />
+          </Link>
+          <Link to={`/update_management/${user.id}`}>
+            <img src={edit} />
+          </Link>
+          <Button style={{ border: 'none', }} className="btn_xoa" onClick={
+            () => handleDelete(user.id)
+          }>
+            <img src={deleteIcon} />
+          </Button>
+        </Space>
+      )
     }));
-    setData([...users]);
-  };
+
+
+    setData([...users])
+
+  }
 
   useEffect(() => {
-    callApi();
-  }, []);
+    callApi()
+  }, [])
+
 
   const columns = [
     {
@@ -74,6 +93,7 @@ function App() {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
+
     },
     {
       title: 'Email',
@@ -94,56 +114,35 @@ function App() {
     {
       title: 'Action',
       key: 'action',
-      render: (_, record) => (
-        <Space size="middle">
-          <Link to="/garage_detail">
-            <img src={eye} style={{ width: '14.05px', height: '16.03px' }} />
-          </Link>
-          <Link to="/update_owner">
-            <img src={edit} />
-          </Link>
-
-          <Link to="/update_management">
-            <img src={deleteIcon} />
-          </Link>
-        </Space>
-      ),
+      dataIndex: 'action',
     },
   ];
 
   return (
     <div className="div">
-      <Space direction="vertical" size="middle">
-        <span>
+      <Space direction="vertical" size="middle" >
+        <span >
           <Space.Compact style={{ width: ' 493px' }}>
             <Select defaultValue="Name" options={options} />
-            <Input
-              placeholder="Search"
-              suffix={<SearchOutlined />}
+            <Input placeholder="Search" suffix={<SearchOutlined />}
               value={search}
-              onChange={e => setSearch(e.target.value)}
+              onChange={
+                (e) => setSearch(e.target.value)
+              }
             />
           </Space.Compact>
-          {/* <Select defaultValue="Status" options={options2} style={{ paddingLeft: '28px', width: ' 296px', }} /> */}
           <Button
-            style={{ backgroundColor: '#8767E1', marginLeft: '10px' }}
-            onClick={() => {
-              callApi();
-            }}
-          >
-            Search
-          </Button>
-          <Button style={{ backgroundColor: '#8767E1', marginLeft: '160px' }}>
-            <Link to="/create_owner">Add owner</Link>
-          </Button>
-          <Button style={{ backgroundColor: '#8767E1', marginLeft: '160px' }}>
-            <Link to="/create_garage">Add garage</Link>
-          </Button>
+            style={{ backgroundColor: '#8767E1', marginLeft: '10px', }}
+            onClick={() => { callApi() }}
+          >Search</Button>
+          <Button style={{ backgroundColor: '#8767E1', marginLeft: '160px', }}>
+            <Link to="/create_garage">Add Owner</Link></Button>
         </span>
       </Space>
       <Table columns={columns} dataSource={data} />
     </div>
-  );
-}
+
+  )
+};
 
 export default App;

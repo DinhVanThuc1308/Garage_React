c2db1b96f5437a75a9bdf7803a6aae16db047514
EnglishCNTT <ngocanha3k52@gmail.com>
2023-05-24 00:13:27 +0700

    done

--- src/components/Table_Garage_Service/index.jsx
+++ src/components/Table_Garage_Service/index.jsx
@@ -10,120 +10,126 @@ import { useParams } from 'react-router-dom';
 import { Button, Input, Select } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
-
 const options = [
-    {
-        value: 'Name',
-        label: 'Name',
-    },
-    {
-        value: 'ID',
-        label: 'ID',
-    },
+  {
+    value: 'Name',
+    label: 'Name',
+  },
+  {
+    value: 'ID',
+    label: 'ID',
+  },
 ];
 const options2 = [
-    {
-        value: 'Status',
-        label: 'Status',
-    },
-    {
-        value: 'active',
-        label: 'done',
-    }
+  {
+    value: 'Status',
+    label: 'Status',
+  },
+  {
+    value: 'active',
+    label: 'done',
+  },
 ];
 
 function App() {
-    const [data, setData] = useState([]);
-    const [search, setSearch] = useState('')
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
 
+  const callApi = async () => {
+    const data = await axiosInstance.get('users');
+    console.log(data);
+    const users = data.map(user => ({
+      id: user.id,
+      name: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      status: user.status === 'active' ? 'Active' : 'Inactive',
+    }));
+    setData([...users]);
+  };
 
-    const callApi = async () => {
+  useEffect(() => {
+    callApi();
+  }, []);
 
-        const data = await axiosInstance.get('users',);
-        console.log(data);
-        const users = data.map(user => ({
-            id: user.id,
-            name: user.fullname,
-            email: user.email,
-            phoneNumber: user.phoneNumber,
-            status: user.status === 'active' ? 'Active' : 'Inactive',
-        }));
-        setData([...users])
+  const columns = [
+    {
+      title: '#',
+      dataIndex: 'id',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Phone number',
+      dataIndex: 'phoneNumber',
+      key: 'phoneNumber',
+    },
 
-    }
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <Space size="middle">
+          <Link to="/service_detail">
+            <img src={eye} style={{ width: '14.05px', height: '16.03px' }} />
+          </Link>
+          <Link to="/update_service">
+            <img src={edit} />
+          </Link>
 
-    useEffect(() => {
-        callApi()
-    }, [])
+          <Link to="/create_service">
+            <img src={deleteIcon} />
+          </Link>
+        </Space>
+      ),
+    },
+  ];
 
-
-    const columns = [
-        {
-            title: '#',
-            dataIndex: 'id',
-        },
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-
-        },
-        {
-            title: 'Email',
-            dataIndex: 'email',
-            key: 'email',
-        },
-        {
-            title: 'Phone number',
-            dataIndex: 'phoneNumber',
-            key: 'phoneNumber',
-        },
-
-        {
-            title: 'Status',
-            dataIndex: 'status',
-            key: 'status',
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: () => (
-                <Space size="middle">
-
-                    <Link to="/service_detail"><img src={eye} style={{ width: '14.05px', height: '16.03px' }} /></Link>
-                    <Link to="/update_service"><img src={edit} /></Link>
-
-                    <Link to="/create_service"><img src={deleteIcon} /></Link>
-                </Space>
-            ),
-        },
-    ];
-
-    return (
-        <div className="div">
-            <Space direction="vertical" size="middle" >
-                <span >
-                    <Space.Compact style={{ width: ' 493px' }}>
-                        <Select defaultValue="Name" options={options} />
-                        <Input placeholder="Search" suffix={<SearchOutlined />}
-                            value={search}
-                            onChange={
-                                (e) => setSearch(e.target.value)
-                            }
-                        />
-                    </Space.Compact>
-                    <Button
-                        style={{ backgroundColor: '#8767E1', marginLeft: '10px', }}
-                        onClick={() => { callApi() }}
-                    >Search</Button>
-                    <Button style={{ backgroundColor: '#8767E1', marginLeft: '160px', }}>
-                        <Link to="/create_service">Add service</Link></Button>
-                </span>
-            </Space>
-            <Table columns={columns} dataSource={data} />
-        </div>
-
-    )
-};
+  return (
+    <div className="div">
+      <Space direction="vertical" size="middle">
+        <span>
+          <Space.Compact style={{ width: ' 493px' }}>
+            <Select defaultValue="Name" options={options} />
+            <Input
+              placeholder="Search"
+              suffix={<SearchOutlined />}
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+            />
+          </Space.Compact>
+          <Button
+            style={{ backgroundColor: '#8767E1', marginLeft: '10px' }}
+            onClick={() => {
+              callApi();
+            }}
+          >
+            Search
+          </Button>
+          <Button style={{ backgroundColor: '#8767E1', marginLeft: '160px' }}>
+            <Link to="/create_service">Add service</Link>
+          </Button>
+          <Button style={{ backgroundColor: '#8767E1', marginLeft: '10px' }}>
+            <Link to="/update_service/2">Update Service</Link>
+          </Button>
+        </span>
+      </Space>
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
+}
 
 export default App;

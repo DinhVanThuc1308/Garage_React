3c1cf70678ac1ee02476d469694c37b1a3ec78e9
PhanDuc2k3 <21011596@st.phenikaa-uni.edu.vn>
2023-05-29 22:49:00 +0700

    update

--- src/components/Update_Profile/index.jsx
+++ src/components/Update_Profile/index.jsx
@@ -10,6 +10,7 @@ import axios from 'axios'
 import axiosInstance from '../../shared/services/http-client.js';
 import { Link, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import dayjs from 'dayjs';
 
 const Update_Profile = () => {
   const { id } = useParams();
@@ -28,10 +29,10 @@ const Update_Profile = () => {
       const response = await axiosInstance.get(`/users/${id}`);
       setUserData(response);
       setDob(response.dob); // Set giá trị mặc định cho dob
-
+      console.log(response.dob);
       setValue('name', response.fullname);
       setValue('email', response.email);
-      setValue('dob', moment(response.dob));
+      setValue('dob', response.dob);
       setValue('phoneNumber', response.phoneNumber);
       setValue('address', response.address);
     } catch (error) {
@@ -48,37 +49,54 @@ const Update_Profile = () => {
     }
   };
 
-  const handleAvatarChange = (info) => {
-    if (info.file.status === 'uploading') {
-      setLoading(true);
-      return;
-    }
-    if (info.file.status === 'done') {
-      setAvatar(info.file.response.avatarUrl);
-      setLoading(false);
-    }
-    if (info.file.status === 'error') {
-      message.error('Failed to upload avatar.');
-      setLoading(false);
-    }
-  };
-
   const uploadAvatar = async (file) => {
+
+    console.log({file});
     try {
       const formData = new FormData();
-      formData.append('files', file);
+      formData.append('files', file.originFileObj);
 
       const response = await axiosInstance.post("upload", formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-
+      console.log(response)
+      setAvatar = response.url;
       return response.avatarUrl;
+      
     } catch (error) {
       throw new Error('Failed to upload avatar.');
     }
   };
+  
+  const handleAvatarChange = (info) => {
+    // console.log(info);
+    // setAvatar(info.file)
+    uploadAvatar(info.file)
+    if (info.file.status === 'uploading') {
+      console.log("uploading")
+      
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === 'done') {
+      setAvatar(info.file.response.avatarUrl );
+      setLoading(false);
+      console.log("done")
+
+    }
+    if (info.file.status === 'error') {
+      // message.error('Failed to upload avatar.');
+      console.log("error")
+      setLoading(false);
+    }
+  };
+
+  const inputStyle = {
+    textAlign: 'center',
+  };
+
 
   const onSubmit = async (formData) => {
     try {
@@ -113,21 +131,21 @@ const Update_Profile = () => {
                 className="avatar-uploader"
                 showUploadList={false}
                 action="https://edison-garage-api.savvycom.xyz/api/upload"
-                beforeUpload={(file) => {
-                  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-                  if (!isJpgOrPng) {
-                    message.error('You can only upload JPG/PNG file!');
-                  }
-                  const isLt2M = file.size / 1024 / 1024 < 2;
-                  if (!isLt2M) {
-                    message.error('Image must be smaller than 2MB!');
-                  }
-                  return isJpgOrPng && isLt2M;
-                }}
+                // beforeUpload={(file) => {
+                //   const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+                //   if (!isJpgOrPng) {
+                //     message.error('You can only upload JPG/PNG file!');
+                //   }
+                //   const isLt2M = file.size / 1024 / 1024 < 2;
+                //   if (!isLt2M) {
+                //     message.error('Image must be smaller than 2MB!');
+                //   }
+                //   return isJpgOrPng && isLt2M;
+                // }}
                 onChange={handleAvatarChange}
               >
                 {avatar ? (
-                  <img src={avatar} alt="avatar" style={{ width: '100%' }} />
+                  <img src={avatar} url={"https://edison-garage-api.savvycom.xyz/"+avatar} alt="avatar" style={{ width: '100%' }} />
                 ) : (
                   <div>
                     {loading ? <LoadingOutlined /> : <PlusOutlined />}
@@ -166,8 +184,9 @@ const Update_Profile = () => {
             <div className="container_updateProfile_inf_input_1">
               <div>
                 <div>Dob</div>
-                <DatePicker className="container_updateProfile_inf_input_dob_phone" 
-                value={moment(dob)} onChange={setDob}
+                <DatePicker className="container_updateProfile_inf_input_dob_phone" inputStyle={inputStyle}
+                value={dayjs(dob)} onChange={setDob}
+                
                 {...register('dob')} />
               </div>
               <div>
@@ -199,10 +218,10 @@ const Update_Profile = () => {
           <div className="line"></div>
           <div className="container.container_updateProfile_button">
             <button className="button_update" style={{ backgroundColor: '#8767E1' }} type="submit">
-              Save
+            <Link to="/view_profile" style={{color: 'white'}}>Save</Link>
             </button>
-            <button className="button_update" style={{ color: '#8767E1' }}>
-              <Link to="/view_profile">Cancel</Link>
+            <button className="button_update" style={{ color: 'purple' }}>
+              <Link to="/view_profile" style={{color: '#8767E1'}}>Cancel</Link>
             </button>
           </div>
         </div>

b04cf0d9d85177eb9e5cbdfcd0f89208d1679a2c
EnglishCNTT <ngocanha3k52@gmail.com>
2023-06-04 17:32:05 +0700

    donesearchupdateowner

--- src/shared/api/updateOwnerAPI.js
+++ src/shared/api/updateOwnerAPI.js
@@ -1,21 +1,20 @@
-import axiosInstance from "../services/http-client";
-
+import axiosInstance from '../services/http-client';
 
 const updateOwnerAPI = {
-    getGarageList: () => {
-        const url = 'garages';
-        return axiosInstance.get(url);
-    },
+  getGarageList: params => {
+    const url = 'garages';
+    return axiosInstance.get(url, { params });
+  },
 
-    getExistingOwnerData: (id, params) => {
-        const url = `users/${id}`;
-        return axiosInstance.get(url, { params });
-    },
+  getExistingOwnerData: (id, params) => {
+    const url = `users/${id}`;
+    return axiosInstance.get(url, { params });
+  },
 
-    updateOwnerData: (data, id) => {
-        const url = `users/${id}`;
-        return axiosInstance.put(url, data);
-    },
+  updateOwnerData: (data, id) => {
+    const url = `users/${id}`;
+    return axiosInstance.put(url, data);
+  },
 };
 
 export default updateOwnerAPI;
